721a19f0c114469d9bf9c74ac52c4bddffede1e2
vseshanka <venkat.pisupate@valuelabs.com>
2019-11-12 08:53:26 +0530

    Updates

---
+++ precache-manifest.ec4d81549a20b22ba0d17ff063c72aa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d36491b5193edd59147b9b36a6dcafc8",
+    "url": "/monsters-search/index.html"
+  },
+  {
+    "revision": "98b62af184ccc10f615f",
+    "url": "/monsters-search/static/css/main.ed94af81.chunk.css"
+  },
+  {
+    "revision": "1843770f1176cb470c61",
+    "url": "/monsters-search/static/js/2.f0ed6f96.chunk.js"
+  },
+  {
+    "revision": "98b62af184ccc10f615f",
+    "url": "/monsters-search/static/js/main.9e434f27.chunk.js"
+  },
+  {
+    "revision": "7c4b20b635c15fa561c8",
+    "url": "/monsters-search/static/js/runtime-main.734c9c42.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monsters-search/static/media/logo.25bf045c.svg"
+  }
+]);
